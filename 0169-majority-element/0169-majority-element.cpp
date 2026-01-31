1508b046ff6adc8d26bb64582050e12cf9ca2769
Prathamesh Kolhe <prathameshkolhe6099@gmail.com>
2026-02-01 02:23:22 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
 
-        float limit=(float)nums.size()/2;
-        int count=1;
-
-        if(nums.size()==1){
-            return nums[0];
-        }
+        int can=0;
+        int count=0;
 
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    count++;
-
-                    if(count>=limit){
-                        return nums[i];
-                    }
-                    
-                }
+            if(count==0){
+                can=nums[i];
             }
-            count=1;
+
+            if(nums[i]==can){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-        return 0;
+
+        return can;
+
+        
     }
 };
