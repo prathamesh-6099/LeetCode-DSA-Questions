d293313ddb3771177b8ef9fc116f8adb9742e45b
Prathamesh Kolhe <prathameshkolhe6099@gmail.com>
2026-08-13 14:45:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.92%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+       
+       int n=matrix[0].size();
+
+       //take transpose
+
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+       }
+
+       //reverse each row
+
+       for(int i=0;i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+       }
+    }
+};
