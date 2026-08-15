a357b136a8675af4d761b9b34a91e6e0f0fa5615
Prathamesh Kolhe <prathameshkolhe6099@gmail.com>
2026-08-16 01:38:56 +0530

    Time: 0 ms (100%), Space: 21 MB (23.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,34 +1,70 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        vector <int> set_row;
-        vector <int> set_col;
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(matrix[i][j]==0){
-                    set_row.push_back(i);
-                    set_col.push_back(j);
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        bool firstRowZero = false;
+        bool firstColZero = false;
+
+        // Check if first row contains 0
+        for(int j = 0; j < col; j++) {
+            if(matrix[0][j] == 0) {
+                firstRowZero = true;
+            }
+        }
+
+        // Check if first column contains 0
+        for(int i = 0; i < row; i++) {
+            if(matrix[i][0] == 0) {
+                firstColZero = true;
+            }
+        }
+
+        // Use first row and first column as markers
+        for(int i = 1; i < row; i++) {
+            for(int j = 1; j < col; j++) {
+
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        //row->0;
-        for(int i=0;i<set_row.size();i++){
-            for(int j=0;j<col;j++){
-                matrix[set_row[i]][j]=0;
+        // Set marked rows to 0
+        for(int i = 1; i < row; i++) {
+            if(matrix[i][0] == 0) {
+
+                for(int j = 1; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
 
-        //col->0;
-        for(int i=0;i<set_col.size();i++){
-            for(int j=0;j<row;j++){
-                matrix[j][set_col[i]]=0;
+        // Set marked columns to 0
+        for(int j = 1; j < col; j++) {
+            if(matrix[0][j] == 0) {
+
+                for(int i = 1; i < row; i++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
 
- 
+        // Finally handle first row
+        if(firstRowZero) {
+            for(int j = 0; j < col; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Finally handle first column
+        if(firstColZero) {
+            for(int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
